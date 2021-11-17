d58c0e4c2e63bf490c4451ef2949439f6265c3f7
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2021-11-17 13:50:15 +0000

    formatting exporter

--- export/textexporter.js
+++ export/textexporter.js
@@ -1,14 +1,17 @@
 class TextExporter {
     export(deck) {
-        let result = deck.phoenixborn.name + '\n';
-        result += 'dice:\n';
+        let result = '**' + deck.phoenixborn.name + '**\n';
         deck.dice.forEach(d => {
             result += d.count + ' ' + d.name + '\n';
         })
-        result += 'cards:\n';
-        deck.cards.sort((a, b) => a.card.type > b.card.type ? 0 : -1)
-        deck.cards.forEach(card => {
-            result += card.count + ' ' + card.card.name + '\n';
+        deck.cards.sort((a, b) => a.card.type > b.card.type ? 0 : -1);
+        let cardType = '';
+        deck.cards.forEach(c => {
+            if (cardType !== c.card.type) {
+                result += '**' + c.card.type + '**\n';
+                cardType = c.card.type;
+            }
+            result += c.count + ' ' + c.card.name + '\n';
         });
         return result;
     }
